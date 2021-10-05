58611e423d28d004214cb425a75e15190fa66a4b
Mukesh Suthar <49620706+mukeshs1510@users.noreply.github.com>
2021-10-05 21:55:28 +0530

    Added Animation while changing Color in the background.

--- app/src/main/java/com/mbs/takenotes/feature_notes/presentation/add_edit_notes/components/notes/AddEditNoteScreen.kt
+++ app/src/main/java/com/mbs/takenotes/feature_notes/presentation/add_edit_notes/components/notes/AddEditNoteScreen.kt
@@ -1,12 +1,21 @@
 package com.mbs.takenotes.feature_notes.presentation.add_edit_notes.components.notes
 
 import androidx.compose.animation.Animatable
-import androidx.compose.material.rememberScaffoldState
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Save
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import com.mbs.takenotes.feature_notes.domain.model.Note
 import com.mbs.takenotes.feature_notes.presentation.add_edit_notes.components.AddEditNoteViewModel
 
 @Composable
@@ -23,8 +32,38 @@ fun AddEditNoteEvent(
 
     val noteBackgroundAnimatable = remember {
         Animatable(
-            Color(noteColor)
+            Color(if (noteColor != -1) noteColor else viewModel.noteColor.value)
         )
+    }
+
+    val scope = rememberCoroutineScope()
+
+    Scaffold(
+        floatingActionButton = {
+            FloatingActionButton(onClick = {
+                viewModel.onEvent(AddEditNoteEvent.SaveNote)
+            },
+                backgroundColor = MaterialTheme.colors.primary
+            ) {
+                Icon(imageVector = Icons.Default.Save, contentDescription = "Save Note")
+            }
+        },
+        scaffoldState = scaffoldState
+    ) {
+        Column(modifier = Modifier
+            .fillMaxSize()
+            .background(noteBackgroundAnimatable.value)
+            .padding(16.dp)
+        ) {
+            Row(
+                modifier = Modifier.fillMaxWidth()
+                    .padding(8.dp)
+            ) {
+                Note.noteColors.forEach{ color -> {
+                    val colorInt = color.toArgb() //2:06:39
+                }}
+            }
+        }
     }
 
 }
